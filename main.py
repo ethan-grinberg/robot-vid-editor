0db25eae4ffbcb8ab5c9e3c00a3927d46cc38142
ethanbg2 <ethanbg2@illinois.edu>
2022-07-10 21:30:08 -0500

    edited main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def index():
     return "Congratulations, it's a web app!"
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+
+app.run(host="127.0.0.1", port=8080, debug=True)
