5dd16c3997c9b0663e71ce0c5df6f793c4cbc2d0
ethanbg2 <ethanbg2@illinois.edu>
2022-07-20 16:23:48 -0500

    refactored code

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-from flask import Flask
-
+from flask import Flask,Response,jsonify, render_template ,logging, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+def home():
+    return "hello world"
 
-
+#run server
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=80)
